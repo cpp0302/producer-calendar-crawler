228da3e17476293136418f57dcdb6cf422344ae9
Toshifumi Seto <toshifumi-seto@live.jp>
2017-01-31 01:51:12 +0900

    開発環境の場合、ページをダウンロードする機能と、すでにダウンロード済みの場合はダウンロード済みファイルを使用してスクレイピングする機能を追加

--- server.js
+++ server.js
@@ -20,9 +20,15 @@ var app = express();
   app.use(express.static(path.join(__dirname, 'public')));
 //});
 
-// app.configure('development', function(){
-//   app.use(express.errorHandler());
-// });
+var env = app.get('env');
+if (env === 'development') {
+	app.set('port', process.env.PORT || 3000);
+	// app.use(express.errorHandler());
+}
+
+if (env === 'production') {
+	app.set('port', process.env.PORT || 8080);
+};
 
 app.get('/', routes.index);
 
@@ -35,18 +41,68 @@ server.listen(app.get('port'), function(){
 });
 
 
+var fs = require('fs');
 var client = require('cheerio-httpcli');
 
-// Googleで「node.js」について検索する。
-client.fetch('http://www.google.com/search', { q: 'node.js' }, function (err, $, res) {
-    // レスポンスヘッダを参照
-    console.log(res.headers);
+function downloadFile(url, filePath, callback) {
 
-    // HTMLタイトルを表示
-    console.log($('title').text());
+	var outfile = fs.createWriteStream(tempFilePath);
 
-    // リンク一覧を表示
-    $('a').each(function (idx) {
-        console.log($(this).attr('href'));
-    });
-});
+	console.log("download url [" + url + "]");
+
+	// 非同期でURLからファイルをダウンロード
+	http.get(url, function(res) {
+		res.pipe(outfile);
+		res.on('end', function() {
+			outfile.close();
+			console.log("url [" + url + "] downloaded.");
+			console.log("file [" + filePath + "]");
+
+			if (typeof callback === 'function') {
+				callback();
+			}
+		});
+	});
+}
+
+function testScraping(url, callback) {
+
+	// Googleで「node.js」について検索する。
+	client.fetch(url, { q: 'node.js' }, function (err, $, res) {
+		// レスポンスヘッダを参照
+		console.log(res.headers);
+
+		// HTMLタイトルを表示
+		console.log($('title').text());
+
+		// リンク一覧を表示
+		$('a').each(function (idx) {
+			console.log($(this).attr('href'));
+		});
+
+		if (typeof callback === 'function') {
+			callback();
+		}
+	});
+}
+
+// 出力先を指定
+var url = "http://idolmaster.jp/schedule/";
+var tempFileName = url.replace(/https{0,1}:\/\//, '').replace(/\//g, '_');
+var tempFilePath = "./public/temp/" + tempFileName;
+var tempFileUrl = "http://localhost:" + app.get('port') + "/temp/" + tempFileName;
+
+if (env === 'development') {
+	if (fs.existsSync(tempFilePath)) {
+		console.log("file [" + tempFileName + "] is already downloaded.");
+		url = tempFileUrl;
+		testScraping(url);
+	} else {
+		downloadFile(url, tempFilePath, function() {
+			url = tempFileUrl;
+			testScraping(url);
+		});
+	}
+} else {
+	testScraping(url);
+}
